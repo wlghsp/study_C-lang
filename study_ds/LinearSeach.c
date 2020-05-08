4fc382315d1f5bf09c434fec22aa61aadad196d1
wlghsp <wlghsp@gmail.com>
2020-05-08 12:03:44 +0900

    first commit

---
+++ study_ds/LinearSeach.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int LSearch(int ar[], int len, int target) // 순차 탐색 알고리즘 적용된 함수
+{
+    int i;
+    for ( i = 0; i < len; i++)
+    {
+        if (ar[i] == target)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
